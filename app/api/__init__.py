3b82797f2625a145c885d6859778497f59fb1f5a
Indrajeet Aditya Roy <roy.indrajeet009@gmail.com>
2025-05-19 18:43:37 -0400

    changes

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.api.datasets import router as datasets_router
-from . import batch
+# from . import batch # Removed batch import
 
 api_router = APIRouter()
 api_router.include_router(datasets_router, tags=["datasets"])
-api_router.include_router(batch.router) 
+# api_router.include_router(batch.router) # Removed batch router 
